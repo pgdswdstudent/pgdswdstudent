fcd9e32db0d34262aeac019bf4cbf98cd8e1de77
pgdswd <pgdswd@lab-foss-pc>
2015-09-29 14:46:30 +0200

    second commit

--- FirstProject/src/pgdswd/NewClass.java
+++ FirstProject/src/pgdswd/NewClass.java
@@ -6,5 +6,9 @@ package pgdswd;
  * @version 1.0
  */
 public class NewClass {
-
+    public static void main(String[] args) {
+        System.out.print("sipho");
+        System.out.print("sipho");
+        System.out.print("sipho");
+    }
 }
